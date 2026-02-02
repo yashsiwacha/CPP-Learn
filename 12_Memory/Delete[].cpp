6f047a6fd628ed1543cddd1aa895c0d387bfded1
Yash Siwach <yashsiwach07@gmail.com>
2026-02-02 17:25:49 +0530

    Memory tasks

---
+++ 12_Memory/Delete[].cpp
@@ -0,0 +1,40 @@
+// Program to demonstrate difference between deletion of dynamically allocated integers and arrays in C++
+#include <iostream>
+using namespace std;
+
+class Item
+{
+    string name;
+
+public:
+    // Constructor - called when object is created
+    Item(string n = "Default")
+    {
+        this->name = n;
+        cout << "The Object " << this->name << " is constructed." << endl;
+    }
+
+    // Destructor - called when object is destroyed
+    ~Item()
+    {
+        cout << "The Object " << this->name << " is destructed." << endl;
+    }
+}; // Missing semicolon fixed
+
+int main()
+{
+    // Single object allocation
+    Item *ptr = new Item("Item1");
+
+    // Array allocation - cannot use initializer list with array of objects
+    // Objects will be default-constructed
+    Item *arr = new Item[5];
+
+    // Delete single object
+    delete ptr;
+
+    // Delete array of objects - calls destructor for each element
+    delete[] arr;
+
+    return 0;
+}
